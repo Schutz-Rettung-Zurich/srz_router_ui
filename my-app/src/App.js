bce384bfa5ffbf6e66ccbec74a8859a70895683e
Christian Nüssli <63254403+chnuessli@users.noreply.github.com>
2022-12-22 14:39:18 +0100

    changes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo_SRZ.png';
 import './App.css';
+import Standard from './standard.json';
+import Emergency from './emergency.json';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>SRZ Router Testing</h1>
       </header>
+      <body className="App-body">
+        <h2>Standard Router</h2>
+      {
+        Records && Records.map( record => {
+          return(
+            <div className="App-box" key={ record.id}>
+              <strong>Status:<i className="green"> {record.result}</i></strong>
+              <br></br>
+              <strong>Reisedistanz:<i className="lightblue"> { record.traveldistance }</i></strong>
+              <br></br>
+              <strong>Reisezeit:<i className="lightblue"> { record.traveltime }</i></strong>
+              <br></br>
+              <strong>Routen-ID:<i className="orange"> { record.route }</i></strong>
+
+            </div>
+          )
+        })
+      }
+              <h2>Blaulicht Router</h2>
+      {
+        Records && Records.map( record => {
+          return(
+            <div className="App-box" key={ record.id}>
+              <strong>Status:<i className="green"> {record.result}</i></strong>
+              <br></br>
+              <strong>Reisedistanz:<i className="lightblue"> { record.traveldistance }</i></strong>
+              <br></br>
+              <strong>Reisezeit:<i className="lightblue"> { record.traveltime }</i></strong>
+              <br></br>
+              <strong>Routen-ID:<i className="orange"> { record.route }</i></strong>
+
+            </div>
+          )
+        })
+      }
+      </body>
+      <footer className="App-footer">
+        2022 NUC
+      </footer>
     </div>
   );
 }
