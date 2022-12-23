bb884089008930a54d4c70981e18854b7120de1c
Christian Nüssli <63254403+chnuessli@users.noreply.github.com>
2022-12-23 10:44:34 +0100

    changes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -11,10 +11,31 @@ function App() {
       </header>
       <body className="App-body">
         <h2>Standard Router</h2>
+        <details>
+        <summary>Ausklappen</summary>
       {
-        Records && Records.map( record => {
+        Standard && Standard.map( record => {
           return(
-            <div className="App-box" key={ record.id}>
+            <div className="App-box-standard" key={ record.id}>
+              <strong>Status:<i className="green"> {record.result}</i></strong>
+              <br></br>
+              <strong>Reisedistanz:<i className="lightblue"> { record.traveldistance }</i></strong>
+              <br></br>
+              <strong>Reisezeit:<i className="lightblue"> { record.traveltime }</i></strong>
+              <br></br>
+              <strong>Routen-ID:<i className="orange"> { record.route }</i></strong>
+            </div>
+          )
+        })
+      }
+      </details>
+        <h2>Blaulicht Router</h2>
+        <details>
+        <summary>Ausklappen</summary>
+      {
+        Emergency && Emergency.map( record => {
+          return(
+            <div className="App-box-emergency" key={ record.id}>
               <strong>Status:<i className="green"> {record.result}</i></strong>
               <br></br>
               <strong>Reisedistanz:<i className="lightblue"> { record.traveldistance }</i></strong>
@@ -27,23 +48,7 @@ function App() {
           )
         })
       }
-              <h2>Blaulicht Router</h2>
-      {
-        Records && Records.map( record => {
-          return(
-            <div className="App-box" key={ record.id}>
-              <strong>Status:<i className="green"> {record.result}</i></strong>
-              <br></br>
-              <strong>Reisedistanz:<i className="lightblue"> { record.traveldistance }</i></strong>
-              <br></br>
-              <strong>Reisezeit:<i className="lightblue"> { record.traveltime }</i></strong>
-              <br></br>
-              <strong>Routen-ID:<i className="orange"> { record.route }</i></strong>
-
-            </div>
-          )
-        })
-      }
+      </details>
       </body>
       <footer className="App-footer">
         2022 NUC
